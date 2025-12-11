8a1975306d4f647fb339e9c68173522579180734
Phon Sovatanak <feelingsleepy4@gmail.com>
2025-12-11 14:19:25 +0700

    Configure deployment and base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     // Basic configuration for vanilla JS project
+    base: '/lomnov_mini_app/',
     server: {
         host: true
     }
