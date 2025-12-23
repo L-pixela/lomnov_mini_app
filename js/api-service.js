2d85659a14cc536f4d52eed13d190b2141a2b455
Phon Sovatanak <feelingsleepy4@gmail.com>
2025-12-23 12:41:35 +0700

    debug the uploading

--- js/api-service.js
+++ js/api-service.js
@@ -112,6 +112,7 @@ export class ApiService {
             const response = await fetch(this.IMAGE_UPLOAD_API, {
                 method: "POST",
                 headers: {
+                    "Content-Type": "multipart/form-data",
                     "ngrok-skip-browser-warning": "true"
                 },
                 body: formData
